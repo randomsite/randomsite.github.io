89d57544fc7bf279dab5677afebbce3873b2b119
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2019-08-21 19:56:16 +0530

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -60,7 +60,7 @@ drone.on('error', error => {
 
 function getRandomName() {
   const adjs = ["randomsite"];
-  const nouns = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"
+  const nouns = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"];
     return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
