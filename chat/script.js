cc986101b73efe15f51f786572e978c58e0f0093
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2019-08-21 20:12:24 +0530

    Update script.js
    added random username assignment

--- chat/script.js
+++ chat/script.js
@@ -61,10 +61,15 @@ drone.on('error', error => {
 function getRandomName() {
   const adjs = ["randomsite"];
   const nouns = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"];
+  var min=1; 
+  var max=2000;  
+  var random = Math.random() * (+max - +min) + +min;
     return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
-    nouns[Math.floor(Math.random() * nouns.length)]
+    nouns[Math.floor(Math.random() * nouns.length)] + random
+    // remove variable random if it doesnt work, like concatenation
+     
   );
 }
 
